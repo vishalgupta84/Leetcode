03ba6059f0080ce6b05671af78fb904c12b2d924
unknown <gupta.vishalarya@gmail.com>
2020-02-08 09:15:42 +0530

    all anagrams in string of a given pattern

---
+++ FindAllAnagramInString.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean match(int[] pattern, int[] temp) {
+        for(int i=0;i<26;i++)
+            if(pattern[i] != temp[i])
+                return false;
+        return true;
+    }
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int[] pattern = new int[26];
+        int[] temp = new int[26];
+        for(int i=0;i<26;i++){
+            temp[i] = pattern[i] = 0;
+        }
+        int m = p.length();
+        int n = s.length();
+        if(n<m)
+            return ans;
+        for(int i=0;i<m;i++){
+            pattern[p.charAt(i)-'a']++;
+            temp[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<=n-m;i++){
+            if(match(pattern,temp))
+                ans.add(i);
+            temp[s.charAt(i)-'a']--;
+            if(i+m < n)
+                temp[s.charAt(i+m)-'a']++;
+        }
+        return ans;
+    }
+}
